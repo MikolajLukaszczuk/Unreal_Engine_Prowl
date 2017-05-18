c1d9098e8f1543fc020f64ad60e60ab87e1865be
Mikolaj Lukaszczuk <mikolaj.lukaszczuk@gmail.com>
2017-05-18 19:28:31 +0200

    Reading Blackboard Data in C++

--- Source/Prowl/ChooseNextWaypoint.cpp
+++ Source/Prowl/ChooseNextWaypoint.cpp
@@ -2,9 +2,13 @@
 
 #include "Prowl.h"
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
 	return EBTNodeResult::Succeeded;
 }
 
--- Source/Prowl/ChooseNextWaypoint.h
+++ Source/Prowl/ChooseNextWaypoint.h
@@ -15,5 +15,8 @@ class PROWL_API UChooseNextWaypoint : public UBTTaskNode
 	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
-	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
 };
