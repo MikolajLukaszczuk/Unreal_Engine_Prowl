96f6a898cc77283e77dc7663a547f3f581cad22d
Mikolaj Lukaszczuk <mikolaj.lukaszczuk@gmail.com>
2017-05-18 21:05:32 +0200

    Composition Over Inheritance

--- Source/Prowl/PatrollingGuard.h
+++ Source/Prowl/PatrollingGuard.h
@@ -13,7 +13,7 @@ class PROWL_API APatrollingGuard : public ATP_ThirdPersonCharacter
 {
 	GENERATED_BODY()
 
-public: // TODO remove or create Get
+public: 
 	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
 	TArray<AActor*> PatrolPointsCPP;
 	
