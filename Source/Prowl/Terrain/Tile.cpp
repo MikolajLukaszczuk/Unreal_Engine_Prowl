ffb8265b25d2c47f5c08583058f217ed10c3b7cf
Mikolaj Lukaszczuk <mikolaj.lukaszczuk@gmail.com>
2017-06-27 20:19:35 +0200

    Spawning Actors In The Tile

--- Source/Prowl/Terrain/Tile.cpp
+++ Source/Prowl/Terrain/Tile.cpp
@@ -12,14 +12,18 @@ ATile::ATile()
 
 }
 
-void  ATile::PlaceActors()
+void  ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int MinSpawn, int MaxSpawn)
 {
 	FVector Min(0, -4000, 0);
 	FVector Max(8000, 4000, 0);
 	FBox Bounds(Min, Max);
-	for (size_t i = 0; i < 20; i++)
+	int NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+	for (size_t i = 0; i < NumberToSpawn; i++)
 	{
 		FVector SpawnPoint = FMath::RandPointInBox(Bounds);
+		AActor *Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
+		Spawned->SetActorRelativeLocation(SpawnPoint);
+		Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
 	}
 }
 
