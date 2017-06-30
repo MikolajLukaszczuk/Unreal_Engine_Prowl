b2bd05a34e4e4cfae158c66a7b682a6881620afe
Mikolaj Lukaszczuk <mikolaj.lukaszczuk@gmail.com>
2017-07-01 01:28:52 +0200

    Sphere Casting C++

--- Source/Prowl/Terrain/Tile.cpp
+++ Source/Prowl/Terrain/Tile.cpp
@@ -2,7 +2,7 @@
 
 #include "Prowl.h"
 #include "Tile.h"
-
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 ATile::ATile()
@@ -32,6 +32,8 @@ void ATile::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	CastSphere(GetActorLocation(), 300);
+	CastSphere(GetActorLocation() + FVector(0, 0, 1000), 300);
 }
 
 // Called every frame
@@ -41,3 +43,21 @@ void ATile::Tick(float DeltaTime)
 
 }
 
+bool ATile::CastSphere(FVector Location, float Radius)
+{
+	FHitResult HitResult;
+	bool HasHit = GetWorld()->SweepSingleByChannel
+	(
+		HitResult,
+		Location,
+		Location,
+		FQuat::Identity,
+		ECollisionChannel::ECC_Camera,
+		FCollisionShape::MakeSphere(Radius)
+
+	);
+
+	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
+	DrawDebugSphere(GetWorld(), Location, Radius, 100, ResultColor, true, 100);
+	return HasHit;
+}
